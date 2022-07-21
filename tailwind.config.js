36cea0f58b0d7bf06de70b123677133379f738c0
luckponte <luckpontecorreia@gmail.com>
2022-07-21 17:13:55 -0300

    Uninstall flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       colors: {
@@ -16,7 +16,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [],
 };
