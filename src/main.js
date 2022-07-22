954d1ec6680e152773e30084be55fe6458aa1a37
luckponte <luckpontecorreia@gmail.com>
2022-07-22 19:32:38 -0300

    Remove unusable links from DOM

--- src/main.js
+++ src/main.js
@@ -1,23 +1,33 @@
-// control mobile menu functions 
+// control mobile menu functions
 let menuOpen = false;
-const mobileMenu = document.querySelector('#navbar-default');
+const mobileMenu = document.querySelector("#navbar-default");
+const androidLink = document.querySelector("#android-link");
+const appleLink = document.querySelector("#apple-link");
 
 document.querySelector("#burger-menu").addEventListener("click", (e) => {
-    menuOpen = !menuOpen;
+  menuOpen = !menuOpen;
 
-    if (menuOpen) {
-        mobileMenu.classList.remove('hidden');
-        mobileMenu.classList.add('block');
-    } else {
-        mobileMenu.classList.remove('block');
-        mobileMenu.classList.add('hidden');
-    }
+  if (menuOpen) {
+    mobileMenu.classList.remove("hidden");
+    mobileMenu.classList.add("block");
+  } else {
+    mobileMenu.classList.remove("block");
+    mobileMenu.classList.add("hidden");
+  }
 });
 
-document.querySelectorAll('.menu-option').forEach((el) => {
-    el.addEventListener("click", (e) => {
-        menuOpen = false;
-        mobileMenu.classList.remove('block');
-        mobileMenu.classList.add('hidden');
-    })
-})
+document.querySelectorAll(".menu-option").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    menuOpen = false;
+    mobileMenu.classList.remove("block");
+    mobileMenu.classList.add("hidden");
+  });
+});
+
+if (!/Android/i.test(navigator.userAgent)) {
+  androidLink.remove();
+}
+
+if (!/iPhone/i.test(navigator.userAgent)) {
+  appleLink.remove();
+}
