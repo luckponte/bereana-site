4f52f31e96e6081bf5bd71864febf7aacb18f9cb
luckponte <luckpontecorreia@gmail.com>
2022-07-29 18:34:19 -0300

    Create first version of prototype

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        "22": "5.5rem"
+      },
+      height: {
+        "22": "5.5rem"
+      },
+      colors: {
+        "main-dark": "#0D4A7C",
+        "main-light": "#026CB6",
+        "highlight-dark": "#F8981D",
+        "highlight-light": "#FFDD00",
+      },
+      backgroundImage: {
+        "head-panel": "url('/src/img/head_panel.jpg')",
+        "content-panel": "url('/src/img/content_panel.jpg')",
+        "links-panel": "url('/src/img/links_panel.jpg')",
+      },
+      boxShadow: {
+        "logo": "0 0 1rem 0.25rem #FFF",
+      },
+    },
+  },
+  plugins: [],
+};
