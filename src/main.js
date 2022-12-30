73f9ab9c0157209abc0ce11b0d4d1db7eb7cb7cf
luckponte <luckpontecorreia@gmail.com>
2022-12-30 16:53:30 +0000

    Implement base contact handler

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 window.addEventListener("load", () => {
+  // know jesus
   document.querySelector("#know-jesus").addEventListener("click", (e) => {
-    console.log('Tem um minuto para ouvir a palavra do Senhor?');
     document.querySelector("#content-trigger-jesus").click();
   });
-  
+
   // control mobile menu functions
   let menuOpen = false;
   const mobileMenu = document.querySelector("#navbar-default");
-  
+
   document.querySelector("#burger-menu").addEventListener("click", (e) => {
     menuOpen = !menuOpen;
-  
+
     if (menuOpen) {
       mobileMenu.classList.remove("hidden");
       mobileMenu.classList.add("block");
@@ -19,12 +19,27 @@ window.addEventListener("load", () => {
       mobileMenu.classList.add("hidden");
     }
   });
-  
+
   document.querySelectorAll(".menu-option").forEach((el) => {
     el.addEventListener("click", (e) => {
       menuOpen = false;
       mobileMenu.classList.remove("block");
       mobileMenu.classList.add("hidden");
     });
+  });
+
+  // Contact formatting and send
+  const form = document.querySelector("#contactForm");
+  function handleContact() {
+    const formData = new FormData(form);
+    window.open(
+      `mailto:luck.ponte.correia@gmail.com?subject=${formData.get("subject")}`,
+      "_self"
+    );
+  }
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleContact();
   });
 });
