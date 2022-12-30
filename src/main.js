01b8a25423afe066a2092899f21445dd2128005c
luckponte <luckpontecorreia@gmail.com>
2022-12-30 16:23:59 +0000

    Make sure page is loaded before defining js routines

--- src/main.js
+++ src/main.js
@@ -1,28 +1,30 @@
-document.querySelector("#know-jesus").addEventListener("click", (e) => {
-  console.log('Tem um minuto para ouvir a palavra do Senhor?');
-  document.querySelector("#content-trigger-jesus").click();
-});
-
-// control mobile menu functions
-let menuOpen = false;
-const mobileMenu = document.querySelector("#navbar-default");
-
-document.querySelector("#burger-menu").addEventListener("click", (e) => {
-  menuOpen = !menuOpen;
-
-  if (menuOpen) {
-    mobileMenu.classList.remove("hidden");
-    mobileMenu.classList.add("block");
-  } else {
-    mobileMenu.classList.remove("block");
-    mobileMenu.classList.add("hidden");
-  }
-});
-
-document.querySelectorAll(".menu-option").forEach((el) => {
-  el.addEventListener("click", (e) => {
-    menuOpen = false;
-    mobileMenu.classList.remove("block");
-    mobileMenu.classList.add("hidden");
+window.addEventListener("load", () => {
+  document.querySelector("#know-jesus").addEventListener("click", (e) => {
+    console.log('Tem um minuto para ouvir a palavra do Senhor?');
+    document.querySelector("#content-trigger-jesus").click();
+  });
+  
+  // control mobile menu functions
+  let menuOpen = false;
+  const mobileMenu = document.querySelector("#navbar-default");
+  
+  document.querySelector("#burger-menu").addEventListener("click", (e) => {
+    menuOpen = !menuOpen;
+  
+    if (menuOpen) {
+      mobileMenu.classList.remove("hidden");
+      mobileMenu.classList.add("block");
+    } else {
+      mobileMenu.classList.remove("block");
+      mobileMenu.classList.add("hidden");
+    }
+  });
+  
+  document.querySelectorAll(".menu-option").forEach((el) => {
+    el.addEventListener("click", (e) => {
+      menuOpen = false;
+      mobileMenu.classList.remove("block");
+      mobileMenu.classList.add("hidden");
+    });
   });
 });
